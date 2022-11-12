1fd208d80b858f570b7112e96892922cd866b109
JC Yan <ahhorsegan@gmail.com>
2022-11-11 21:56:17 -0800

    Added jsdocs to workflow and a js file to test

---
+++ source/jsdoctest.js
@@ -0,0 +1,10 @@
+/**
+ * This sets the value of the second integer to the first.
+ * @param {int} parameter1 - first integer 
+ * @param {int} parameter2 - second integer
+ * @return {int} first integer who's value has been set to second.
+ */
+function jsDocTest(parameter1, parameter2){
+    parameter1 = parameter2;
+    return parameter1;
+}
