e99d3ccb25622733cc21d6087c0291288ad69fbf
Tyler Le <tylerle99@yahoo.com>
2022-11-14 09:31:38 -0800

    remove semicolons and convert to single quotes

--- source/assets/scripts/jobcard.js
+++ source/assets/scripts/jobcard.js
@@ -1,14 +1,14 @@
 // Run the init() function when the page has loaded
-window.addEventListener("DOMContentLoaded", init);
+window.addEventListener ('DOMContentLoaded', init)
 
-function init() {
-  progressBarListener();
+function init () {
+  progressBarListener()
 }
 
 /**
  * @description Updates progress bar by retrieving which step was clicked and updates the coloring of all steps
  */
-function progressBarListener() {
+function progressBarListener () {
   // prettier-ignore
   for (let i = 0; i < document.getElementsByClassName('stages').length; i++) {
     document
@@ -31,15 +31,15 @@ function progressBarListener() {
  */
 function updateProgress(ul, stepNum) {
   // get the specific progress bar
-  const li = ul.getElementsByTagName("li");
+  const li = ul.getElementsByTagName('li')
 
   // change each bubble accordingly
   for (let i = 0; i < li.length; i++) {
     if (i === stepNum - 1) {
-      li[i].classList.add("active");
-      console.log(`Updated stage ${i + 1}`);
+      li[i].classList.add('active')
+      console.log(`Updated stage ${i + 1}`)
     } else {
-      li[i].classList.remove("active");
+      li[i].classList.remove("active")
     }
   }
 }
