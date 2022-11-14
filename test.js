cacfc907d9934d30ef93484f665d63e947630af9
Tyler Le <tylerle99@yahoo.com>
2022-11-14 11:50:18 -0800

    added eslint

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-console.log('Double Quotes with no semicolon')
-console.log('Double Quotes with semicolon')
+console.log("Double Quotes with no semicolon")
+console.log("Double Quotes with semicolon");
 console.log('Single Quotes with no semicolon')
-console.log('Single Quotes with semicolon')
+console.log('Single Quotes with semicolon');
