6a15e3cd34729b7c9e48249601c1cda9fb23ce84
Tyler Le <tylerle99@yahoo.com>
2022-11-16 09:26:28 -0800

    fixed showModal bug

--- source/assets/scripts/edit_popup.js
+++ source/assets/scripts/edit_popup.js
@@ -1,18 +1,18 @@
-const edit_dialog = document.getElementsByClassName('edit-application');
+const edit_dialog = document.getElementsByClassName('edit-application')[0];
 const edit_cancel = document.getElementsByClassName('edit_cancel')
 
 for (let i = 0; i < edit_cancel.length; i++) {
   edit_cancel[i].addEventListener("click", () => {
-    edit_dialog[i].close();
+    edit_dialog.close();
   });
 }
 
 
 var editIcons = document.getElementsByClassName("edit-icon");
-
+console.log(edit_dialog)
 for (let i = 0; i < editIcons.length; i++) {
   editIcons[i].addEventListener("click", () => {
     console.log(`${i}th item wants to be edited`);
-    edit_dialog[i].showModal();
+    edit_dialog.showModal();
   });
 }
