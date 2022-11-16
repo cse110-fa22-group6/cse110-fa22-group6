4230618f215259f27559ce40aa592bffcc041dfc
Tyler Le <tylerle99@yahoo.com>
2022-11-15 22:32:56 -0800

    integrate delete popup and job card

--- source/assets/scripts/delete_popup.js
+++ source/assets/scripts/delete_popup.js
@@ -1,11 +1,17 @@
 const deleteBtn = document.getElementById('deletebtn');
 const delete_dialog = document.getElementById('delete-application');
 
+
 document.getElementById('d_cancel').addEventListener('click', () => {
     delete_dialog.close();
-    reset();
 });
 
-deleteBtn.addEventListener('click', () => {
+
+var deleteIcons = document.getElementsByClassName("delete-icon");
+
+for (let i = 0; i < deleteIcons.length; i++) {
+  deleteIcons[i].addEventListener("click", () => {
+    console.log(`${i}th item wants to be deleted`);
     delete_dialog.showModal();
-});
+  });
+}
