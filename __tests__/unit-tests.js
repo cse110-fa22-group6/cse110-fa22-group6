c4b24f263f6bcadb3d1f989df2c28771ae24548f
andreijsecor <andreijsecor@gmail.com>
2022-11-26 23:12:15 -0800

    added text, fixed issues that came up during push

--- __tests__/unit-tests.js
+++ __tests__/unit-tests.js
@@ -53,14 +53,33 @@ describe("Basic user flow for Website", () => {
         expect(jobs.length).toBe(1);
     });
 
-  it("Check add confirmation popup", async () => {
-    console.log("Testing add confirmation popup...");
-    var addBtn = await page.$('[id="add_application_btn"]');
-    await addBtn.click(0, 1, 1);
-    var popup = await page.$('[id="add-application"]');
-    expect(await popup.isIntersectingViewport()).toBe(true);
-    var cancelBtn = await popup.$('[id="add_cancel"]');
-    await cancelBtn.click(0, 1, 1);
-    expect(await popup.isIntersectingViewport()).toBe(false);
-  });
+    it("Check new job card", async () => {
+        console.log("Checking the new job card...");
+        var jobs = await page.$$('job-card');
+        expect(jobs.length).toBe(1);
+        var app = jobs[0];
+        var root = await app.getProperty('shadowRoot');
+
+        var position = await root.$('.position');
+        var positionText = await position.getProperty('innerText');
+        var positionVal = await positionText.jsonValue();
+        expect(positionVal).toBe('Professional Burger Flipper');
+
+        var company = await root.$('.company');
+        var companyText = await company.getProperty('innerText');
+        var companyVal = await companyText.jsonValue();
+        expect(companyVal).toBe('Ronald\'s Emporium');
+
+        var location = await root.$('.location');
+        var locationText = await location.getProperty('innerText');
+        var locationVal = await locationText.jsonValue();
+        expect(locationVal).toBe('Jackson, CA');
+
+        var activeStatus = await root.$('.active');
+        var statusBlock = await activeStatus.$('p');
+        var statusText = await statusBlock.getProperty('innerText');
+        var statusVal = await statusText.jsonValue();
+        expect(statusVal).toBe('Unapplied');
+    });
+
 });
