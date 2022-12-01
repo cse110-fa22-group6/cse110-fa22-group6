099c3d7a6a50b6bb28e983ea795127635056bd0d
tamarasagakova <78051761+tamarasagakova@users.noreply.github.com>
2022-11-30 19:48:40 -0800

    Update source/assets/scripts/edit_popup.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- source/assets/scripts/edit_popup.js
+++ source/assets/scripts/edit_popup.js
@@ -1,5 +1,5 @@
 const edit_dialog = document.getElementsByClassName("edit-application")[0];
-const edit_cancel = document.getElementsByClassName('edit_cancel')
+const edit_cancel = document.getElementsByClassName("edit_cancel");
 
 for (let i = 0; i < edit_cancel.length; i++) {
   edit_cancel[i].addEventListener("click", () => {
